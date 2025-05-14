d35baffb61bb6571a3843308a1128e6a0b49c8ec
Bagus Sadewo <bgsdewoo@gmail.com>
2025-05-14 10:33:33 +0700

    delete workspace

--- resources/js/Pages/Workspaces/EditWorkspace.jsx
+++ resources/js/Pages/Workspaces/EditWorkspace.jsx
@@ -5,7 +5,7 @@ import { Button } from '@/Components/ui/button';
 import { Card, CardContent } from '@/Components/ui/card';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/Components/ui/select';
 import { flashMessage } from '@/lib/utils';
-import { useForm } from '@inertiajs/react';
+import { router, useForm } from '@inertiajs/react';
 import { toast } from 'sonner';
 export default function Editworkspace({ workspace, page_settings, visibilities }) {
     const { data, setData, processing, errors, reset, post } = useForm({
@@ -93,13 +93,37 @@ export default function Editworkspace({ workspace, page_settings, visibilities }
                             </div>
                         </div>
                     </div>
-                    <div className="flex items-center justify-end gap-x-2 py-6">
-                        <Button type="button" variant="ghost" onClick={() => reset()}>
-                            Reset
+                    <div className="flex items-center justify-between gap-x-2 py-6">
+                        <Button
+                            type="button"
+                            variant="link"
+                            className="font-medium text-red-500 hover:text-red-600 hover:no-underline"
+                            onClick={() =>
+                                router.delete(
+                                    route('workspaces.destroy', {
+                                        workspace: workspace,
+                                    }),
+                                    {
+                                        preserveScroll: true,
+                                        preserveState: true,
+                                        onSuccess: (success) => {
+                                            const flash = flashMessage(success);
+                                            if (flash) toast[flash.type](flash.message);
+                                        },
+                                    },
+                                )
+                            }
+                        >
+                            Delete Workspace
                         </Button>
-                        <Button variant="red" type="submit" disabled={processing}>
-                            Save
-                        </Button>
+                        <div className="flex gap-x-2">
+                            <Button type="button" variant="ghost" onClick={() => reset()}>
+                                Reset
+                            </Button>
+                            <Button variant="red" type="submit" disabled={processing}>
+                                Save
+                            </Button>
+                        </div>
                     </div>
                 </form>
             </CardContent>
