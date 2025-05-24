2c9d49f186ed7e90e631d6495114e8ea46bc7bc8
Bagus Sadewo <bgsdewoo@gmail.com>
2025-05-24 14:38:56 +0700

    tampilkan stastitik dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,11 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Response;
-
+use App\Models\User;
+use App\Models\Member;
+use App\Models\Workspace;
+use App\Models\Card;
+use App\Enums\CardStatus;
 class DashboardController extends Controller
 {
  public function index():Response
  {
-    return inertia(component:'Dashboard');
+    return inertia('Dashboard',[
+        'page_settings' => [
+            'title' => 'Dashboard',
+            'subtitle' => 'You can see a summary of the information here',
+        ],
+        'count' => [
+            'users' => fn() => User::count(),
+            'workspaces' => fn() => Member::query()
+            ->where('members.user_id',request()->user()->id)
+            ->whereHasMorph('memberable', Workspace::class)
+            ->count(),
+        'tasks' => fn() => Member::query()
+            ->where('members.user_id',request()->user()->id)
+            ->whereHasMorph('memberable', Card::class)
+            ->count(),
+        'dones' => fn() => Member::query()
+            ->where('members.user_id',request()->user()->id)
+            ->whereHasMorph('memberable', Card::class,
+              fn($query) => $query->where('status', CardStatus::DONE->value)
+            )
+            ->count(),
+        ]
+    ]);
  }
 }
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,6 +1,46 @@
+import Header from '@/Components/Header';
+import Widget from '@/Components/Widget';
 import AppLayout from '@/Layouts/AppLayout';
+import { usePage } from '@inertiajs/react';
+import { PiBriefcase, PiSquaresFour } from 'react-icons/pi';
 
-export default function Dashboard() {
-    return <div>This is a Dashboard</div>;
+export default function Dashboard({ count, page_settings }) {
+    const auth = usePage().props.auth.user;
+    return (
+        <>
+            <div>
+                <div className="xl:pr-96">
+                    <div>
+                        <Header title={page_settings.title} subtitle={page_settings.subtitle} />
+                        <div>
+                            <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+                                <Widget
+                                    icon={<PiBriefcase className="h-6 w-6 text-white" />}
+                                    title="Workspace"
+                                    count={count.workspaces}
+                                    bgColor="bg-gradient-to-b from-green-400 to-green-600"
+                                />
+                                <Widget
+                                    icon={<PiSquaresFour className="h-6 w-6 text-white" />}
+                                    title="My Tasks"
+                                    count={count.tasks}
+                                    bgColor="bg-gradient-to-b from-fuchsia-400 to-fuchsia-600"
+                                />
+                                <Widget
+                                    icon={<PiBriefcase className="h-6 w-6 text-white" />}
+                                    title="Tasks Done"
+                                    count={count.dones}
+                                    bgColor="bg-gradient-to-b from-sky-400 to-sky-600"
+                                />
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <aside className="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto border-l border-gray-200 px-4 py-6 lg:px-8 xl:block">
+                {/* card */}
+            </aside>
+        </>
+    );
 }
-Dashboard.layout = (page) => <AppLayout children={page} title="Dashboard" />;
+Dashboard.layout = (page) => <AppLayout children={page} title={page.props.page_settings.title} />;
