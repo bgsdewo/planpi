8fa56f570d64669350fb179e16a7f09c0183f9e2
Bagus Sadewo <bgsdewoo@gmail.com>
2025-05-26 13:21:59 +0700

    landing page

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,7 @@ use App\Http\Controllers\MyTaskController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Welcome');
 });
 
 Route::get('dashboard', [DashboardController::class, 'index'])
